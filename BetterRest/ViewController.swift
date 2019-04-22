1caa62764afd1ff5510d8ec64b547a1f0bc8ec93
Jay <jay.ambrish.raval@gmail.com>
2019-04-22 01:56:46 -0400

    Initial Commit

---
+++ BetterRest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BetterRest
+//
+//  Created by Jay Raval on 4/22/19.
+//  Copyright © 2019 Jay Raval. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
